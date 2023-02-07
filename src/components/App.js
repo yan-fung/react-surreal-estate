619572660d1406e3214d025d11d3000c3ce6aa74
Yan Fung <fungmingyan@hotmail.com>
2023-02-07 08:45:07 +0000

    Tidied up files, added Airbnb style guide and installed Prettier

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,11 @@ function App() {
   return (
     <div className="App">
       <h2>Surreal Estate</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+        nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+        volutpat.
+      </p>
     </div>
   );
 }
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import { render, screen } from "@testing-library/react";
 import App from "../components/App";
 
-it('renders correctly', () => {
+it("renders correctly", () => {
   render(<App />);
   const linkElement = screen.getByText(/Surreal Estate/i);
   expect(linkElement).toBeInTheDocument();
