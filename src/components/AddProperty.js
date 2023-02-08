aeffdd72a66eb70f9b620a5ffc4c3d8fe5aa7ce7
Yan Fung <fungmingyan@hotmail.com>
2023-02-08 18:13:53 +0000

    Added AddProperty component and the header

---
+++ src/components/AddProperty.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/properties.css";
+
+const AddProperty = () => {
+  return (
+    <div className="property_header">
+      <h1>Add Properties</h1>
+    </div>
+  );
+};
+
+export default AddProperty;
