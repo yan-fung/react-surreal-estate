99b6dea9b0f324b3b2f74d78a12da9eb8478c290
Yan Fung <fungmingyan@hotmail.com>
2023-02-08 18:21:46 +0000

    Created Properties component and added a header

---
+++ src/components/Properties.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/properties.css";
+
+const Properties = () => {
+  return (
+    <div className="property_header">
+      <h1>Properties Page</h1>
+    </div>
+  );
+};
+
+export default Properties;
