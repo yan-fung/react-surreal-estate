b4650f9add313c5db90840512ba5598084f49cd3
Yan Fung <fungmingyan@hotmail.com>
2023-02-08 18:16:44 +0000

    Added Route path and passed Properties and AddProperty components as props

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar";
+import Properties from "./Properties";
+import AddProperty from "./AddProperty";
 import "../styles/app.css";
 
-const App = () => (
-  <div className="App">
-    <NavBar />
-  </div>
-);
+const App = () => {
+  return (
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Properties />} />
+        <Route path="/add_property" element={<AddProperty />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
