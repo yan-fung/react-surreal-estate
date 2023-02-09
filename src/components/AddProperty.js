c1bbd5eb859a1aefe9abfcb9d7a18d489d2a0298
Yan Fung <fungmingyan@hotmail.com>
2023-02-09 10:16:29 +0000

    Added initialState variable to store the initial state for different fields. Added fields state variable for updating fields state. Added event handlers for handling add property and field change. Added a form with title input and added a button for submission

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,10 +1,43 @@
-import React from "react";
-import "../styles/properties.css";
+import React, { useState } from "react";
+import "../styles/add-property.css";
 
 const AddProperty = () => {
+  const initialState = {
+    fields: {
+      title: "",
+      city: "Manchester",
+    },
+  };
+  /* eslint-disable */
+  const [fields, setFields] = useState(initialState.fields);
+
+  const handleAddProperty = (event) => {
+    event.preventDefault();
+    /* eslint-disable */
+    console.log(fields);
+  };
+
+  const handleFieldChange = (event) => {
+    setFields({ ...fields, [event.target.name]: event.target.value });
+  }
+
   return (
-    <div className="property_header">
+    <div className="add-property">
       <h1>Add Properties</h1>
+      <form onSubmit={handleAddProperty}>
+        <label htmlFor="title">
+          Title
+            <input
+              id="title"
+              name="title"
+              value={fields.title}
+              onChange={handleFieldChange}
+            />
+        </label>
+        <button className="button" type="submit">
+          Add
+        </button>
+      </form>
     </div>
   );
 };
