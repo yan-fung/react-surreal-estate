ec6e0b1c21d6dc37e810a23748e99e38d57514a3
Yan Fung <fungmingyan@hotmail.com>
2023-02-12 22:56:22 +0000

    Used if statement to return the message as null if it is not truthy. Used ternary operator to render the correct icon for the success and error message

---
+++ src/components/Alert.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Proptypes from "prop-types";
+import "../styles/alert.css";
+// import check from "../styles/images/check.png";
+import checkIcon from "../styles/images/check-icon.png";
+import errorIcon from "../styles/images/error-icon.png";
+
+const Alert = ({ message, success }) => {
+  if (!message) {
+    return null;
+  }
+
+  return (
+    <div className={`alert-${success ? "success" : "error"}`}>
+      {message === "Property added." ? (
+        <img
+          style={{ height: "30px", width: "30px", background: "transparent" }}
+          alt="check"
+          src={checkIcon}
+        />
+      ) : (
+        <img
+          style={{ height: "30px", width: "30px", background: "transparent" }}
+          alt="error"
+          src={errorIcon}
+        />
+      )}
+      {message}
+    </div>
+  );
+};
+
+export default Alert;
+
+Alert.defaultProps = {
+  success: false,
+};
+
+Alert.propTypes = {
+  message: Proptypes.string.isRequired,
+  success: Proptypes.bool,
+};
