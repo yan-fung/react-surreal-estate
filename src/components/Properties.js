e7d9f739d0cac3572aaeee374ac8ac25e1fb6f3f
Yan Fung <fungmingyan@hotmail.com>
2023-02-15 16:45:21 +0000

    Added properties and alert state variables in useEffect for setting state and catch the error message. Passed two props - message and success in Alert component. Passed eight props in PropertyCard component

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import PropertyCard from "./PropertyCard";
+import Alert from "./Alert";
 import "../styles/properties.css";
 
 const Properties = () => {
+  const [properties, setProperties] = useState([]);
+  const [alert, setAlert] = useState({ message: "" });
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/api/v1/PropertyListing")
+      .then((res) => {
+        setProperties(res.data);
+        setAlert({ message: "" });
+      })
+      .catch(() => {
+        setAlert({
+          message: "Server error, please try again later.",
+        });
+      });
+  }, []);
+
   return (
     <div className="property_header">
-      <h1>Properties Page</h1>
+      <h1>View Properties</h1>
+      <Alert message={alert.message} success={alert.isSuccess} />
+      <div className="properties">
+        {properties.map((property) => (
+          <div className="item" key={property._id}>
+            <PropertyCard
+              key={property._id}
+              title={property.title}
+              bathrooms={property.bathrooms}
+              bedrooms={property.bedrooms}
+              type={property.type}
+              price={property.price}
+              email={property.email}
+              city={property.city}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
