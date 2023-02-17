6b9db3e8967611a06573a7a8eb6f19059a9285b4
Yan Fung <fungmingyan@hotmail.com>
2023-02-17 12:14:54 +0000

    Added useLocation hook and used the  prop search to trigger useEffect to make a new request to API. Render SideBar and PropertyCard components and passes props

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import axios from "axios";
 import PropertyCard from "./PropertyCard";
 import Alert from "./Alert";
+import SideBar from "./SideBar";
 import "../styles/properties.css";
 
 const Properties = () => {
   const [properties, setProperties] = useState([]);
   const [alert, setAlert] = useState({ message: "" });
+  const { search } = useLocation();
 
   useEffect(() => {
     axios
@@ -22,25 +25,36 @@ const Properties = () => {
       });
   }, []);
 
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/api/v1/PropertyListing${search}`)
+      .then(({ data }) => setProperties(data))
+      .catch((err) => console.error(err));
+  }, [search]);
+
   return (
     <div className="property_header">
-      <h1>View Properties</h1>
       <Alert message={alert.message} success={alert.isSuccess} />
-      <div className="properties">
-        {properties.map((property) => (
-          <div className="item" key={property._id}>
-            <PropertyCard
-              key={property._id}
-              title={property.title}
-              bathrooms={property.bathrooms}
-              bedrooms={property.bedrooms}
-              type={property.type}
-              price={property.price}
-              email={property.email}
-              city={property.city}
-            />
-          </div>
-        ))}
+      <div className="property_container">
+        <div className="sidebar">
+          <SideBar />
+        </div>
+        <div className="properties">
+          {properties.map((property) => (
+            <div className="item" key={property._id}>
+              <PropertyCard
+                key={property._id}
+                title={property.title}
+                bathrooms={property.bathrooms}
+                bedrooms={property.bedrooms}
+                type={property.type}
+                price={property.price}
+                email={property.email}
+                city={property.city}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
