ff8a072538c869ea2d67215340d0a7d2a3fcc5ad
Yan Fung <fungmingyan@hotmail.com>
2023-02-18 09:59:18 +0000

    Created a search form and added a handleSearch event handler for making a new API request by using useNavigate. Updated buildQueryString function so that it can support more than one query key

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,19 +1,25 @@
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import qs from "qs";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import "../styles/sidebar.css";
 
 const SideBar = () => {
-  const buildQueryString = (operation, valueObj) => {
-    const { search } = useLocation();
+  const { search } = useLocation();
+  const [query, setquery] = useState();
 
+  const buildQueryString = (operation, valueObj) => {
     const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
-    console.log("currentQueryParams", currentQueryParams);
+    // console.log("currentQueryParams: ", currentQueryParams);
     const newQueryParams = {
       ...currentQueryParams,
-      [operation]: JSON.stringify(valueObj),
+      [operation]: JSON.stringify({
+        ...JSON.parse(currentQueryParams[operation] || "{}"),
+        ...valueObj,
+      }),
     };
-    console.log("newQueryParams", newQueryParams);
+    // console.log("newQueryParams: ", newQueryParams);
 
     return qs.stringify(newQueryParams, {
       addQueryPrefix: true,
@@ -21,8 +27,30 @@ const SideBar = () => {
     });
   };
 
+  const navigate = useNavigate();
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const newQueryString = buildQueryString("query", {
+      title: { $regex: query },
+    });
+    navigate(newQueryString);
+  };
+
   return (
     <div className="sidebar">
+      <form className="search-box" onSubmit={handleSearch}>
+        <input
+          className="search-input"
+          type="text"
+          value={query}
+          onChange={(e) => setquery(e.target.value)}
+          placeholder="Search"
+        />
+        <button type="submit" className="button">
+          <FontAwesomeIcon icon={faMagnifyingGlass} className="icon-search" />
+        </button>
+      </form>
       <div className="sidebar_city">
         <h3>Filter by City</h3>
         <ul>
