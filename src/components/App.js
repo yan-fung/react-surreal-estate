43a64adddc4c9184394d057e83d3af33b06dcd9f
Yan Fung <fungmingyan@hotmail.com>
2023-02-18 18:54:09 +0000

    Created handleLogin and handleLogout functions and passed it as props to NavBar components

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 import Properties from "./Properties";
@@ -6,9 +6,22 @@ import AddProperty from "./AddProperty";
 import "../styles/app.css";
 
 const App = () => {
+  const [userId, setUserId] = useState("");
+
+  const handleLogin = (response) => {
+    console.log(response);
+    setUserId(response.userID);
+  };
+
+  const handleLogout = () => {
+    setUserId("");
+    window.FB.logout();
+    console.log("Log out successfully");
+  };
+
   return (
     <div className="App">
-      <NavBar />
+      <NavBar onLogin={handleLogin} onLogout={handleLogout} userId={userId} />
       <Routes>
         <Route path="/" element={<Properties />} />
         <Route path="/add_property" element={<AddProperty />} />
