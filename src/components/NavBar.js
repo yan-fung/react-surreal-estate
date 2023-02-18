44a35b7d314cab48d535c262e5515af73568e2cf
Yan Fung <fungmingyan@hotmail.com>
2023-02-18 19:01:57 +0000

    Used conditional operator to display log in or log out options

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,12 @@
 import React from "react";
+import FacebookLogin from "react-facebook-login";
+import { FaFacebookF } from "react-icons/fa";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import homeImage from "../styles/images/home.png";
 import "../styles/navbar.css";
 
-const NavBar = () => {
+const NavBar = ({ onLogin, onLogout, userId }) => {
   return (
     <div className="navbar">
       <img src={homeImage} alt="home_image" />
@@ -19,8 +22,29 @@ const NavBar = () => {
           </Link>
         </li>
       </ul>
+      {userId ? (
+        <button className="sign-out-btn" type="button" onClick={onLogout}>
+          Sign out
+        </button>
+      ) : (
+        <div className="fb-login">
+          <FacebookLogin
+            appId="813549316783868"
+            callback={onLogin}
+            userId={userId}
+            cssClass="my-facebook-button-class"
+            icon={<FaFacebookF className="fb-icon" />}
+          />
+        </div>
+      )}
     </div>
   );
 };
 
 export default NavBar;
+
+NavBar.propTypes = {
+  onLogin: PropTypes.func.isRequired,
+  onLogout: PropTypes.func.isRequired,
+  userId: PropTypes.string.isRequired,
+};
